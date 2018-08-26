2f9c6f9f6344ce2664b62bf8a58f49efe6047350
Alessandro Avila <alavil@outlook.com>
2018-08-26 16:34:28 +0200

    Added CalculatorApplication (V1, V2, V3).

---
+++ Chapter-2/CalculatorApplication/V1/CalculatorWeb/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using CalculatorService.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using System;
+
+namespace CalculatorWeb.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<int> Add(int a, int b)
+        {
+            var calculatorClient = ServiceProxy.Create<ICalculatorService>(
+                new Uri("fabric:/CalculatorApplication/CalculatorService"));
+            return calculatorClient.Add(a, b).Result;
+        }
+
+        [HttpGet]
+        public ActionResult<int> Subtract(int a, int b)
+        {
+            var calculatorClient = ServiceProxy.Create<ICalculatorService>(
+                new Uri("fabric:/CalculatorApplication/CalculatorService"));
+            return calculatorClient.Subtract(a, b).Result;
+        }
+    }
+}
---
+++ Chapter-2/CalculatorApplication/V3/CalculatorService.Interfaces/ICalculatorService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace CalculatorService.Interfaces
+{
+    [ServiceContract]
+    public interface ICalculatorService : IService
+    {
+        [OperationContract]
+        Task<string> Add(int a, int b);
+        [OperationContract]
+        Task<string> Subtract(int a, int b);
+    }
+}
